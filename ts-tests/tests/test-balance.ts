b91e4d7b0e1f0a43970d58478cfb54621c85d84e
Sulaman0 <symikhan70@gmail.com>
2025-02-21 19:12:08 +0500

    Pass test case for : genesis balance is setup correctly

--- ts-tests/tests/test-balance.ts
+++ ts-tests/tests/test-balance.ts
@@ -8,7 +8,7 @@ describeWithFrontier("Frontier RPC (Balance)", (context) => {
     const TEST_ACCOUNT = "0xdd33Af49c851553841E94066B54Fd28612522901";
     const TEST_ACCOUNT_PRIVATE_KEY = "0x4ca933bffe83185dda76e7913fc96e5c97cdb7ca1fbfcc085d6376e6f564ef71";
     const TRANFER_VALUE = "0x200"; // 512, must be higher than ExistentialDeposit
-    const GAS_PRICE = "0x3B9ACA00"; // 1000000000
+    const GAS_PRICE = "0x4A817C80"; // 1000000000
     var nonce = 0;
 
     step("genesis balance is setup correctly", async function () {
