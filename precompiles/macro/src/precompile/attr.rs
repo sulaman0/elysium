fa41b9e7ac2a86e7a64810fea7e19830ef623559
Sulaman0 <symikhan70@gmail.com>
2025-02-27 12:32:44 +0500

    Fix precompile attribute file.

--- precompiles/macro/src/precompile/attr.rs
+++ precompiles/macro/src/precompile/attr.rs
@@ -16,7 +16,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 use proc_macro2::Span;
 use quote::ToTokens;
 use syn::spanned::Spanned;
